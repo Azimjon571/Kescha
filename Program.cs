b24e08ed23134a06d218bc012bc0bde3a9f79305
Azimjon <azimjon.007@mail.ru>
2022-09-23 14:58:02 +0200

    INFRA: Upload Project

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿System.Console.WriteLine("Ismingizni kiriting:");
+string name = Console.ReadLine();
+string greeting = $"Assalomu Aleykum, {name} men Keschaman";
+System.Console.WriteLine(greeting);
+
+System.Console.WriteLine("Yoshingizni kiriting: ");
+int Age = Convert.ToInt32(Console.ReadLine());
+int keschAge = 5;
+int ageDifference = Age - keschAge;
+System.Console.WriteLine($"Bizning yoshimiz o`tasidagi farq {ageDifference} yosh ekan.");

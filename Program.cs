c8d0d87f2257e32728af241ea2ee7a9f153701cd
Azimjon <azimjon.007@mail.ru>
2022-09-28 14:53:49 +0200

    INFRA: Change language and add new feature

--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
-﻿System.Console.WriteLine("Ismingizni kiriting:");
+﻿System.Console.Write("Please enter your name:");
 string name = Console.ReadLine();
-string greeting = $"Assalomu Aleykum, {name} men Keschaman";
+string greeting = $" Hello, {name} my name is Kescha";
 System.Console.WriteLine(greeting);
 
-System.Console.WriteLine("Yoshingizni kiriting: ");
-int Age = Convert.ToInt32(Console.ReadLine());
-int keschAge = 5;
-int ageDifference = Age - keschAge;
-System.Console.WriteLine($"Bizning yoshimiz o`tasidagi farq {ageDifference} yosh ekan.");
+System.Console.Write("Enter your age: ");
+int age = Convert.ToInt32(Console.ReadLine());
+int keschasAge = 5;
+int ageDifference = age - keschasAge;
+System.Console.WriteLine($"The difference between your and Kescha`s age is {ageDifference}.");
+System.Console.WriteLine($"You are older than KESCHA: {age > keschasAge}");
+System.Console.WriteLine($"You are younger than KESCHA: {age < keschasAge}");
+System.Console.WriteLine($"You are older than KESCHA: {age >= keschasAge}");

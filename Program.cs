8d3b8ce386ac686132b8740ee8d6a5684f1d7e74
Azimjon <azimjon.007@mail.ru>
2022-09-29 10:07:42 +0200

    IF conditions

--- Program.cs
+++ Program.cs
@@ -1,10 +1,29 @@
-﻿System.Console.WriteLine("Ismingizni kiriting:");
+﻿System.Console.Write("Please enter your name:");
 string name = Console.ReadLine();
-string greeting = $"Assalomu Aleykum, {name} men Keschaman";
+string greeting = $" Hello, {name} my name is Kescha";
 System.Console.WriteLine(greeting);
 
-System.Console.WriteLine("Yoshingizni kiriting: ");
-int Age = Convert.ToInt32(Console.ReadLine());
-int keschAge = 5;
-int ageDifference = Age - keschAge;
-System.Console.WriteLine($"Bizning yoshimiz o`tasidagi farq {ageDifference} yosh ekan.");
+System.Console.Write("Enter your age: ");
+int age = Convert.ToInt32(Console.ReadLine());
+int keschasAge = 5;
+int ageDifference = age - keschasAge;
+
+System.Console.WriteLine($"The difference between your and Keschas age is {ageDifference}.");
+if(age > keschasAge)
+{
+    System.Console.WriteLine("You are older than KESCHA");
+}
+else if(age == keschasAge)
+{
+    System.Console.WriteLine("You are equal.");
+}
+else if(age<keschasAge)
+{
+   System.Console.WriteLine("KESCHA older then you."); 
+}
+
+else
+{
+    System.Console.WriteLine("You are younger");
+}
+

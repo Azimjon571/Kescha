96758895c9f8737c6665a9cc70dc3d7fc08650e0
Azimjon <azimjon.007@mail.ru>
2022-10-09 17:45:33 +0200

    Add Class

---
+++ Classes/Animal.cs
@@ -0,0 +1,34 @@
+namespace KESCHA.Classes
+{
+    class Animal
+    {
+        public string Name { get; set; }
+        public int Age {get; set; }
+
+        public int AgeDifference { get; set; }
+
+        public Animal()
+        {
+            Name = "No name";
+            Age=0;
+            AgeDifference=0;
+        }
+        
+        public Animal(
+            string name, 
+            int userAge, 
+            int animalAge)
+        {
+            Name=name;
+            Age=animalAge;
+            CalculateAgerDifference( userAge, animalAge);
+        }
+
+        private void CalculateAgerDifference(int userAge, int animalAge)
+        {
+            AgeDifference = userAge-animalAge;
+        }
+
+    }    
+}
+
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿string password = "";
+﻿using KESCHA.Classes;
+
+string password = "";
 do
 {
     System.Console.WriteLine("Enter the Password:");
@@ -6,25 +8,26 @@ do
 }while(password != "12344321");
 
 System.Console.Write("Please enter your name:");
-string name = Console.ReadLine();
-string greeting = $" Hello, {name} my name is Kescha";
+string NName = Console.ReadLine();
+string greeting = $" Hello, {NName} my name is Kescha";
 System.Console.WriteLine(greeting);
 
 System.Console.Write("Enter your age: ");
 int age = Convert.ToInt32(Console.ReadLine());
-int keschasAge = 5;
-int ageDifference = age - keschasAge;
 
-System.Console.WriteLine($"The difference between your and Keschas age is {ageDifference}.");
-if(age > keschasAge)
+Animal kescha =new Animal(name: "KESCHA", age, animalAge:5); 
+
+System.Console.WriteLine($"The difference between your and Keschas age is {kescha.AgeDifference}.");
+
+if(age > kescha.Age)
 {
     System.Console.WriteLine("You are older than KESCHA");
 }
-else if(age == keschasAge)
+else if(age == kescha.Age)
 {
     System.Console.WriteLine("You are equal.");
 }
-else if(age<keschasAge)
+else if(age<kescha.Age)
 {
    System.Console.WriteLine("KESCHA older then you."); 
 }
